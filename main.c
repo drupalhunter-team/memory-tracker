411c9e053a883f216599edf71040ad7b76052ca5
yinuoyang <yyn4a55@hotmail.com>
2014-11-02 01:23:47 -0500

    main file for memory trackers

---
+++ main.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mhDefs.h"
+#include "stuDefs.h"
+
+//Struct a new type called heaptype
+
+//This function initalize heaptype in main function
+void mh_init(HeapType *heap);
+
+//A memory allocation prototype. Free the memory allocated
+void mh_cleanup(HeapType *heap);
+
+/*add a new block to block array, save the address, return
+memory using malloc()
+*/
+void *mh_alloc(HeapType *heap, int n, char *label);
+
+
+/*The function finds the blocks element in heap parameter corresponds to the block of memory pointed by addr
+it sets the block element to not reserverd and deallocates the memory pointed by addr using free();
+*/
+void mh_dealloc(HeapType *heap, void *addr);
+
+/*
+*/
+int mh_count(Heap *heap);
+
+/*A screen dumping function with prototype:
+*/
+void nh_dump(HeapType *heap);
+
+/*A garbage collection function with prototype
+using free()*/
+void mh_collect(HeapType *heap);
+
+
+int main()
+{
+  HeapType *heap;
+  void  *ptr1, *ptr2, *ptr3, *ptr4;
+
+  heap = (HeapType *) malloc(sizeof(HeapType));
+  mh_init(heap);
+
+  ptr1 = mh_alloc(heap, 5*sizeof(int), "ints");
+  ptr2 = mh_alloc(heap, 10*sizeof(double), "doubles");
+  ptr3 = mh_alloc(heap, 8*sizeof(char), "chars");
+  ptr4 = mh_alloc(heap, 12*sizeof(StudentType), "Students");
+
+  printf("\nDUMP 1, byte count = %d\n", mh_count(heap));
+  mh_dump(heap);
+
+  mh_dealloc(heap, ptr1);
+  mh_dealloc(heap, ptr2);
+  mh_dealloc(heap, ptr3);
+
+  printf("\nDUMP 2, byte count = %d\n", mh_count(heap));
+  mh_dump(heap);
+
+  mh_collect(heap);
+
+  printf("\nDUMP 3, byte count = %d\n", mh_count(heap));
+  mh_dump(heap);
+  printf("\n\n");
+
+  mh_cleanup(heap);
+  free(heap);
+
+  return 0;
+}
+
+void mh_init(HeapType *heap){	
+  HeapType newHeap;
+  heap -> newHeap;
+
+  newHeap.numBlocks = MAX_BLK;
+
+  assert(newHeap.numBlocks != MAX_BLK);
+
+  newHeap.blocks = (BlockType *)malloc(MAX_BLK*sizeof(BlockType));
+  };
+
+void mh_cleanup(HeapType *heap){
+  free(heap.blocks);
+};
+
+void *mh_alloc(HeapType *heap, int n, char *label){
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
